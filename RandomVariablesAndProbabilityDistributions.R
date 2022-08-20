342887f043d0a818f135e207efbf5537ea57dd62
AhmetCETINKAYA6006049 <6006049@bakircay.edu.tr>
2022-08-20 22:51:36 +0300

    Random Variables examples completed

--- RandomVariablesAndProbabilityDistributions.R
+++ RandomVariablesAndProbabilityDistributions.R
@@ -27,3 +27,46 @@ mean(sample(population,12))
 
 mean(control)
 mean(treatment)
+
+
+
+#Exercises
+library(downloader) 
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
+filename <- basename(url)
+download(url, destfile=filename)
+x <- unlist( read.csv(filename) )
+x
+
+RNGkind("Mersenne-Twister", "Inversion", "Rejection")
+
+#1 What is the average of these weights?
+mean(x)
+
+# Take a random sample of size 5.
+# What is the absolute value (use abs()) of the difference 
+# between the average of the sample and the average of all the values?
+
+set.seed(1)
+
+ms <- mean(sample(x,5))
+ms
+mx <- mean(x)
+mx
+diff <- ms - mx
+diff
+abs(diff)
+
+#3 After setting the seed at 5, set.seed(5), take a random sample of size 5. 
+#  What is the absolute value of the difference between the average of the sample and 
+#  the average of all the values?
+
+set.seed(5)
+
+ms <- mean(sample(x,5))
+ms
+mx <- mean(x)
+mx
+diff <- ms - mx
+diff
+abs(diff)
