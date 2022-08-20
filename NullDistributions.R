fbce347df405059898b101a9e4005187369e3b72
AhmetCETINKAYA6006049 <6006049@bakircay.edu.tr>
2022-08-20 23:33:28 +0300

    Null Distribution Exercises completed

--- NullDistributions.R
+++ NullDistributions.R
@@ -47,15 +47,52 @@ mean(abs(nulls) > obs)
 
 # Exercises
 
+#Here x represents the weights for the entire population.
+library(downloader) 
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleControlsPopulation.csv"
+filename <- basename(url)
+download(url, destfile=filename)
+x <- unlist( read.csv(filename) )
 
 
+#1 Set the seed at 1, then using a for-loop take a random sample of 5 mice 1,000 times. 
+#Save these averages.
+#What proportion of these 1,000 averages are more than 1 gram away from the average of x ?
+
+set.seed(1)
 
 
+n <- 1000
+avgs <- vector("numeric", n)
+distance <- 0
+yes <- 0
+for(i in 1:n) {
+  samps <- sample(x,5)
+  avgs[i] <- mean(samps)
+  
+}
+hist(avgs)
+
+mean(abs(avgs - mean(x)) > 1)
 
 
+#2 We are now going to increase the number of times we redo the sample from 1,000 to 10,000. 
+#Set the seed at 1, then using a for-loop take a random sample of 5 mice 10,000 times. 
+#Save these averages.
+#What proportion of these 10,000 averages are more than 1 gram away from the average of x ?
+
+set.seed(1)
 
 
+n <- 10000
+avgs <- vector("numeric", n)
+distance <- 0
+yes <- 0
+for(i in 1:n) {
+  samps <- sample(x,5)
+  avgs[i] <- mean(samps)
+  
+}
+hist(avgs)
 
-
-
-
+mean(abs(avgs - mean(x)) > 1)
